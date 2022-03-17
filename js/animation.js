f188f933f738c52ae96bdb889eafc190dae007df
CatPusheen <prosto_svetka@inbox.ru>
2022-03-17 12:53:58 +0300

    новый вариант

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,4 @@
-const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+/*const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 function increaseNumberAnimationStep(i, element, endNumber) {
     if (i <= endNumber) {
         if (i === endNumber) {
@@ -20,5 +20,24 @@ function initIncreaseNumberAnimation() {
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
+initIncreaseNumberAnimation();*/
 
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input'); 
+ 
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text"; 
+      
+    formContainer.appendChild(input);
+    document.querySelector('#form').insertBefore(formContainer, document.querySelector('.form__submit')); // Задание 3
+  }
+ 
+  const otherInput = document.querySelector('.form__other-input');
+  if (event.target.value !== 'other' && otherInput) { 
+  document.querySelector('#form').removeChild(otherInput); 
+  }
+});
